b60a70c441d6d526b0aeb9ff057f0929504047cc
orestevitali <oreste.vitali@outlook.it>
2025-09-22 22:42:23 +0200

    modifiche 22 settembre A

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Sensor tempSensor = new Sensor("Temperature", 25);
 tempSensor.DisplayInfo();
-tempSensor.Value = 24.0;
 tempSensor.DisplayInfo();
 Sensor pressureSensor = new Sensor("Pressure", 1.01);
 pressureSensor.DisplayInfo();
-Console.WriteLine("Hello, World!");
+var defaultSensor = new Sensor();
+tempSensor.GenerateQrId();
+pressureSensor.GenerateQrId();
 
+var qrIDTempSensor = tempSensor.GetQrId();
+var qrIDPressureSensor = pressureSensor.GetQrId();
 
 
--- Sensor.cs
+++ Sensor.cs
@@ -1,10 +1,14 @@
 
 
+using System.ComponentModel;
+using System.Security.Cryptography;
+
 public class Sensor
 {
     private static double SerialNumber = 0.0; // Static field to keep track of serial numbers
-    public string Name { get; set; }
-    public double Value { get; set; }
+    private string Name { get; set; }
+    private double Value { get; set; }
+    private string _qrId;
 
     public Sensor(string name, double value)
     {
@@ -12,6 +16,37 @@ public class Sensor
         Value = value;
         SerialNumber += 0.1; // Increment serial number for each new instance
         Console.WriteLine($"Sensor Created with Serial Number: {SerialNumber:F1}");
+    }
+
+    public Sensor()
+    {
+        Name = "DefaultSensor";
+        Value = 0.0;
+        SerialNumber += 0.1; // Increment serial number for each new instance
+        Console.WriteLine($"Sensor Created with Serial Number: {SerialNumber:F1}");
+    }
+
+    public void GenerateQrId()
+    {
+        if (string.IsNullOrEmpty(_qrId))
+        {
+            _qrId = RandomNumberGenerator.GetInt32(100000, 999999).ToString();
+        }
+        else
+        {
+            Console.WriteLine("QR ID already generated.");
+        }
+    }
+
+    public string GetQrId(){
+        if (string.IsNullOrEmpty(_qrId))
+        {
+            Console.WriteLine("QR ID not generated yet.");
+            _qrId = "";
+        }
+
+
+        return _qrId;
     }
 
     public void DisplayInfo()
